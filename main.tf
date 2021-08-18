6cb270136d3364b917e08952032c36df8b75e3bc
重田 将人 <masato.0928.j@gmail.com>
2021-08-18 21:32:32 +0900

    initial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.54.0"
+    }
+  }
+
+  backend "s3" {
+    region = "ap-northeast-1"
+    bucket = "ft-state-mst"
+    key    = "terraform.tfstate"
+  }
+}
