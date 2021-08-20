9ff9039fda9328f5cc14351ca057cb8b801a1c02
重田 将人 <masato.0928.j@gmail.com>
2021-08-20 20:51:19 +0900

    vpc作成

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.54.0"
-    }
-  }
-
-  backend "s3" {
-    region = "ap-northeast-1"
-    bucket = "ft-state-mst"
-    key    = "terraform.tfstate"
-  }
+module "network" {
+  source = "./network"
 }
