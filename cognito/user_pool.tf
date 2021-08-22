5798b16b4db9ab0b860e6c1ad8a9b70f6610dd1c
重田 将人 <masato.0928.j@gmail.com>
2021-08-22 10:43:24 +0900

    terraform fmt

--- cognito/user_pool.tf
+++ cognito/user_pool.tf
@@ -1,55 +1,55 @@
 resource "aws_cognito_user_pool" "user_pool" {
-    auto_verified_attributes  = [
-        "email",
-    ]
-    mfa_configuration         = "OFF"
-    name                      = "user_pool"
+  auto_verified_attributes = [
+    "email",
+  ]
+  mfa_configuration = "OFF"
+  name              = "user_pool"
 
-    account_recovery_setting {
-        recovery_mechanism {
-            name     = "verified_email"
-            priority = 1
-        }
-        recovery_mechanism {
-            name     = "verified_phone_number"
-            priority = 2
-        }
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
     }
+    recovery_mechanism {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+  }
 
-    admin_create_user_config {
-        allow_admin_create_user_only = false
-    }
-    email_configuration {
-        email_sending_account = "COGNITO_DEFAULT"
-    }
+  admin_create_user_config {
+    allow_admin_create_user_only = false
+  }
+  email_configuration {
+    email_sending_account = "COGNITO_DEFAULT"
+  }
 
-    password_policy {
-        minimum_length                   = 8
-        require_lowercase                = true
-        require_numbers                  = true
-        require_symbols                  = true
-        require_uppercase                = true
-        temporary_password_validity_days = 7
-    }
+  password_policy {
+    minimum_length                   = 8
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = true
+    require_uppercase                = true
+    temporary_password_validity_days = 7
+  }
 
-    schema {
-        attribute_data_type      = "String"
-        developer_only_attribute = false
-        mutable                  = true
-        name                     = "email"
-        required                 = true
+  schema {
+    attribute_data_type      = "String"
+    developer_only_attribute = false
+    mutable                  = true
+    name                     = "email"
+    required                 = true
 
-        string_attribute_constraints {
-            max_length = "2048"
-            min_length = "0"
-        }
+    string_attribute_constraints {
+      max_length = "2048"
+      min_length = "0"
     }
+  }
 
-    username_configuration {
-        case_sensitive = false
-    }
+  username_configuration {
+    case_sensitive = false
+  }
 
-    verification_message_template {
-        default_email_option = "CONFIRM_WITH_CODE"
-    }
+  verification_message_template {
+    default_email_option = "CONFIRM_WITH_CODE"
+  }
 }
